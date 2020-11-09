e4b77258c68d2e5e8121cf2f1d1411ef72c2c9f4
Meet <meetkakadiya710@gmail.com>
2020-11-09 09:07:27 +0530

    bulk added

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -8,6 +8,10 @@ const Book = db.collection('bookData')
 const invenotry = db.collection('inventory')
 const order = db.collection('orderData');
 
+const student = db.collection('studentData');
+
+const Item = db.collection('item');
+
 const addUser = async (req, res) => {
 
     let user = req.body
@@ -15,6 +19,31 @@ const addUser = async (req, res) => {
     let userData = await User.insert({ user });
     res.json({
         userData
+    })
+}
+
+const addStudent = async (req, res) => {
+
+    let studentData = await student.insert([
+        {
+            "_id": 4,
+            "grades": [
+                { "grade": 80, "mean": 75, "std": 8 },
+                { "grade": 85, "mean": 90, "std": 6 },
+                { "grade": 85, "mean": 85, "std": 8 }
+            ]
+        },
+        {
+            _id: 5,
+            grades: [
+                { grade: 80, mean: 75, std: 8 },
+                { grade: 85, mean: 90, std: 5 },
+                { grade: 90, mean: 85, std: 3 }
+            ]
+        }
+    ]);
+    res.json({
+        studentData
     })
 }
 
@@ -82,11 +111,73 @@ const findData = async (req, res) => {
 
 }
 
+const updateStudent = async (req, res) => {
+
+    let data = await student.updateOne(
+        { _id: 1, grades: 82 },
+        { $set: { "grades.$": 60 } }
+        // {
+        //     _id: 5,
+        //     grades: { $elemMatch: { grade: { $lte: 90 }, mean: { $gt: 80 } } }
+        //   },
+        //   { $set: { "grades.$.std" : 10 } }
+    );
+    res.json({ data })
+}
+
+const getstudent = async (req, res) => {
+    let data = await student.find({}).toArray()
+    res.json({ data })
+}
+
+const addItem = (req, res) => {
+
+    var bulk = Item.initializeOrderedBulkOp()
+
+    bulk.insert({ item: "abc123", defaultQty: 100, status: "A", points: 100 });
+    bulk.insert({ item: "ijk123", defaultQty: 200, status: "A", points: 200 });
+    bulk.insert({ item: "mop123", defaultQty: 0, status: "P", points: 0 });
+    bulk.execute(function(err, data){
+        // console.log(data)
+        res.json({data})
+    });
+
+}
+
+const updateBulk = (req, res) => {
+
+    var bulk = Item.initializeOrderedBulkOp()
+    bulk.find( { status: "I" } ).update( { $set: { status: "M", points: "100" } } );
+    bulk.find( { item: "abc123" } ).replaceOne( { item: "abc123", status: "P", points: 100 } )
+
+    bulk.execute(function(err, data){
+        // console.log(data)
+        res.json({ data})
+    })
+
+}
+
+const itemdata = async (req, res) => {
+    let data = await Item.find({}).toArray()
+    res.json({data})
+}
+
+const removeBulk = (req, res) => {
+
+    var bulk = Item.initializeOrderedBulkOp()
+    bulk.find( { status: "P" } ).remove()
+
+    bulk.execute(function(err, data){
+        // console.log(data)
+        res.json({ data})
+    })
+}
 
 
 
 
 module.exports = {
-    addUser, addBook, filterdata, allUser, findData
+    addUser, addBook, filterdata, allUser, findData, addStudent, updateStudent, getstudent, addItem, updateBulk,
+    itemdata, removeBulk
 
 }
--- router/user.js
+++ router/user.js
@@ -10,11 +10,29 @@ router.post('/user',userCtrl.addUser)
 
 router.post('/book', userCtrl.addUser)
 
+router.post('/student', userCtrl.addStudent)
+
+router.post('/update',userCtrl.updateStudent)
+
+router.post('/bupdate', userCtrl.updateBulk);
+
+router.post('/item', userCtrl.addItem)
+
+router.post('/bremove', userCtrl.removeBulk)
+
 router.get('/data', userCtrl.filterdata)
 
 router.get('/alluser',userCtrl.allUser)
 
 router.get('/all', userCtrl.findData)
+
+router.get('/allstd',userCtrl.getstudent)
+
+router.get('/allitem', userCtrl.itemdata)
+
+
+
+
 
 
 module.exports = router
